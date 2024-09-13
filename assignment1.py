6f3fc5d17b4bc905167372f69721819c7818685e
davis011235 <157519728+davis011235@users.noreply.github.com>
2024-09-12 20:01:20 -0500

    added from prophet import Prophet

--- assignment1.py
+++ assignment1.py
@@ -1,6 +1,5 @@
 import pandas as pd
-import numpy as np
-import prophet
+from prophet import Prophet
 
 data = pd.read_csv("https://raw.githubusercontent.com/dustywhite7/econ8310-assignment1/main/assignment_data_train.csv")
 
@@ -8,10 +7,11 @@ prophdat = data[['Timestamp', 'trips']]
 prophdat.Timestamp = pd.to_datetime(prophdat.Timestamp)
 prophdat = pd.DataFrame(prophdat.values, columns = ['ds','y'])
 
-model = Prophet(daily_seasonality=True)
+model = Prophet()
 modelFit = model.fit(prophdat)
-
-future = modelFit.make_future_dataframe(freq= 'H', periods=744)
+future = modelFit.make_future_dataframe(freq = 'H', periods=744)
 forecast = modelFit.predict(future)
 
 pred = forecast[-744:]['daily']
+
+print(pred)
