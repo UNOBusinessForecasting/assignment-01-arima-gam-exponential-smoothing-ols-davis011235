a7e346a26b5c04840ffe9e316427457beba7c29c
davis011235 <157519728+davis011235@users.noreply.github.com>
2024-09-17 20:54:33 -0500

    switched to m/d/h instead of y/m/d

--- assignment1.py
+++ assignment1.py
@@ -3,10 +3,10 @@ import pandas as pd
 
 data = pd.read_csv("https://raw.githubusercontent.com/dustywhite7/econ8310-assignment1/main/assignment_data_train.csv")
 new_data = pd.read_csv("https://raw.githubusercontent.com/UNOBusinessForecasting/assignment-01-arima-gam-exponential-smoothing-ols-davis011235/refs/heads/main/assignment_data_test.csv")
-new_data = new_data[['year', 'month', 'day']]
+new_data = new_data[['month', 'day', 'hour']]
 
 data.head()
-x= data[['year', 'month', 'day']]
+x= data[[ 'month', 'day','hour']]
 y = data['trips']
 
 model = LinearGAM(s(0) + s(1) + s(2))
